7ee55ebb834f1ae8619b18593ff1810ae7eb6829
Bollapragada Sairam <bollapragadasairam@Bollapragadas-MacBook-Pro.local>
2025-10-04 20:40:51 +0530

    Position all headings exactly 98px from top

--- components/JobsSection.js
+++ components/JobsSection.js
@@ -38,7 +38,7 @@ export default function JobsSection() {
   ];
 
   return (
-    <section className="py-8 pt-24 mt-20 bg-gradient-to-br from-blue-50 to-indigo-50" id="careers">
+    <section className="py-8 mt-20 bg-gradient-to-br from-blue-50 to-indigo-50" id="careers" style={{ paddingTop: '98px' }}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-8 pt-8">
